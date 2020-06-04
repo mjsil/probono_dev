f3317ad007fe98481a48037ac8b7b207ec06feb5
mjsil <m.j.silveira@outlook.com>
2020-06-04 11:10:01 -0300

    Front-End | inicial

---
+++ Front-End/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'prettier',
+        'prettier/react',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'class-methods-use-this': 'off',
+        'no-param-reassign': 'off',
+        'react/jsx-filename-extension': [
+            'warn',
+            { extensions: ['.js', '.jsx'] },
+        ],
+        'import/prefer-default-export': 'off',
+        'jsx-quotes': ['error', 'prefer-single'],
+    },
+};
