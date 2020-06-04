9816205e425fd34b993d65639c099da94797aac1
mjsil <m.j.silveira@outlook.com>
2020-06-04 11:52:05 -0300

    Controller Process

---
+++ Back-End/src/app/controllers/ProcessController.js
@@ -0,0 +1,43 @@
+import User from '../models/User';
+import Process from '../models/Process';
+
+class ProcessController {
+    async store(req, res) {
+        const { number, id_client } = req.body;
+
+        const isLawyer = await User.findOne({
+            where: {
+                id: req.userId,
+                provider: true,
+            },
+        });
+
+        if (!isLawyer) {
+            return res.status(401).json({
+                error: 'You can only create process with lawyer',
+            });
+        }
+
+        const clientExists = await User.findOne({
+            where: {
+                id: id_client,
+            },
+        });
+
+        if (!clientExists) {
+            return res.status(401).json({
+                error: 'Client not found',
+            });
+        }
+
+        const process = await Process.create({
+            number,
+            id_lawyer: req.userId,
+            id_client,
+        });
+
+        return res.json(process);
+    }
+}
+
+export default new ProcessController();
