d0773f28f33e25a51781efde15651518ca31d6bd
mjsil <m.j.silveira@outlook.com>
2020-06-07 13:04:54 -0300

    Back-End v2.01

--- Back-End/src/app/models/Progress.js
+++ Back-End/src/app/models/Progress.js
@@ -6,8 +6,6 @@ class Progress extends Model {
             {
                 generic_title: Sequelize.STRING,
                 details: Sequelize.STRING,
-                date: Sequelize.DATE,
-                hours: Sequelize.STRING,
             },
             {
                 sequelize,
